ffe7a926e3c9eb05826ee4ebb3d8cf4beb899837
Katia Aresti <karesti@redhat.com>
2024-06-14 18:13:15 +0200

    start

--- README.md
+++
@@ -1,2 +0,0 @@
-# quarkus-insights-demo-2024
-This is the repository holding the code for the Quarkus and Infinispan Insights Demo
---
+++ src/main/java/org/infinispan/bounded/DeveloperSchema.java
@@ -0,0 +1,10 @@
+package org.infinispan.bounded;
+
+import org.infinispan.protostream.GeneratedSchema;
+import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
+
+@AutoProtoSchemaBuilder(includeClasses = { Developer.class },
+      schemaFileName = "developers-schema.proto",
+      schemaPackageName = "insights")
+public interface DeveloperSchema extends GeneratedSchema {
+}
